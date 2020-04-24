b0c2ba088061759b277303d9820cbd690e2ce8d0
Bojan Gabric <bojan@bojangabric.com>
2020-04-24 10:52:40 +0200

    Add reducers

---
+++ resources/js/reducers/index.js
@@ -0,0 +1,29 @@
+import { OPEN_MODAL, CLOSE_MODAL, ADD_TO_CART } from '../constants/action-types';
+
+const initialState = {
+  openModal: false
+};
+
+function rootReducer(state = initialState, action) {
+  if (action.type === OPEN_MODAL) {
+    return {
+      ...state,
+      modalPizza: action.payload,
+      openModal: true
+    };
+  } else if (action.type === CLOSE_MODAL) {
+    return {
+      ...state,
+      openModal: false
+    };
+  } else if (action.type === ADD_TO_CART) {
+    return {
+      ...state,
+      openModal: false,
+      cart: [...state.cart, action.payload]
+    };
+  }
+  return state;
+}
+
+export default rootReducer;
