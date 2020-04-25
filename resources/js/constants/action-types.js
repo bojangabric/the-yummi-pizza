61ddaa49b77404a7b67f21cffa13040a6e603710
Bojan Gabric <bojan@bojangabric.com>
2020-04-25 19:36:36 +0200

    Add action types

--- resources/js/constants/action-types.js
+++ resources/js/constants/action-types.js
@@ -2,3 +2,5 @@ export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const GET_ALL_PIZZAS = 'GET_ALL_PIZZAS';
+export const CONFIRM_ORDER = 'CONFIRM_ORDER';
