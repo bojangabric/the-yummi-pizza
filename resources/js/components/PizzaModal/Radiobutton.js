d05022671724b116a636d56c50961a36131c0c51
Bojan Gabric <bojan@bojangabric.com>
2020-04-25 10:23:41 +0200

    Remove checked from Radiobutton

--- resources/js/components/PizzaModal/Radiobutton.js
+++ resources/js/components/PizzaModal/Radiobutton.js
@@ -1,17 +1,10 @@
 import React from 'react';
 
-const Radiobutton = ({ size, checked, onChange }) => {
+const Radiobutton = ({ size, onChange }) => {
   return (
     <div>
       <label className="bg-white flex shadow p-4 items-center rounded text-sm">
-        <input
-          className="form-radio"
-          type="radio"
-          name="size"
-          checked={checked}
-          value={size}
-          onChange={e => onChange(e)}
-        />
+        <input className="form-radio" type="radio" name="size" value={size} onChange={e => onChange(e)} />
         <span className="ml-2">{size}</span>
       </label>
     </div>
