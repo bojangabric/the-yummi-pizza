0d1353684af4176e477c93f1952e7eeb51c51544
Bojan Gabric <bojan@bojangabric.com>
2020-04-25 19:39:01 +0200

    Get pizzas from database

--- resources/js/views/Index.js
+++ resources/js/views/Index.js
@@ -1,67 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Pizza from '../components/Pizza';
 import Cart from '../components/Cart';
 import PizzaModal from '../components/PizzaModal';
+import { connect } from 'react-redux';
+import { getAllPizzas } from '../actions';
 
-const Index = () => {
-  return (
-    <div className="w-3/5 mx-auto">
-      <Header />
+class Index extends Component {
+  componentDidMount() {
+    this.props.getAllPizzas();
+  }
 
-      <div className="text-cesnter py-12 text-4xl">Choose a pizza you'd like to order!</div>
+  render() {
+    return (
+      <div className="w-3/5 mx-auto">
+        <Header />
 
-      <div className="flex">
-        <div className="w-3/4 grid grid-cols-3 gap-10 self-start">
-          <Pizza
-            name="Bresaola"
-            description="Tomato sauce, mozzarella, rocket, beef prosciutto, grana padano, olive oil"
-            price="15.00"
-          />
-          <Pizza
-            name="Proscuitto"
-            description="Tomato sauce, mozzarella, rocket, prosciutto, cherry tomato, olive oil"
-            price="12.00"
-          />
-          <Pizza
-            name="Soprano"
-            description="Tomato sauce, gauda, nduja piccante, chili pepper, onion, basil, olive oil"
-            price="10.00"
-          />
-          <Pizza name="Margherita" description="Tomato sauce, mozzarella, basil, olive oil" price="10.00" />
-          <Pizza
-            name="Primavera"
-            description="Pesto sauce, ricotta, mozzarella, zucchini, cherry tomato, basil, olive oil"
-            price="10.00"
-          />
-          <Pizza name="Viola" description="Tomato sauce, brie, bacon, radicchio, olive oil" price="10.00" />
-          <Pizza
-            name="Bresaola"
-            description="Tomato sauce, mozzarella, rocket, beef prosciutto, grana padano, olive oil"
-            price="15.00"
-          />
-          <Pizza
-            name="Proscuitto"
-            description="Tomato sauce, mozzarella, rocket, prosciutto, cherry tomato, olive oil"
-            price="12.00"
-          />
-          <Pizza
-            name="Soprano"
-            description="Tomato sauce, gauda, nduja piccante, chili pepper, onion, basil, olive oil"
-            price="10.00"
-          />
+        <div className="text-cesnter py-12 text-4xl">Choose a pizza you'd like to order!</div>
+
+        <div className="flex">
+          <div className="w-3/4 grid grid-cols-3 gap-10 self-start">
+            {this.props.pizzas.map(pizza => (
+              <Pizza name={pizza.name} description={pizza.description} price={pizza.price} key={pizza.id} />
+            ))}
+          </div>
+          <div className="w-1/4">
+            <Cart />
+          </div>
         </div>
-        <div className="w-1/4">
-          <Cart />
-        </div>
+
+        <PizzaModal />
+
+        <Footer />
       </div>
+    );
+  }
+}
 
-      <PizzaModal />
+const mapStateToProps = state => ({
+  pizzas: state.pizzas
+});
 
-      <Footer />
-    </div>
-  );
-};
-
-export default Index;
+export default connect(mapStateToProps, { getAllPizzas })(Index);
