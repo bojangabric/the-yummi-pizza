7d1dd51197574d8691c0c149b17269cac4a6e93a
Bojan Gabric <bojan@bojangabric.com>
2020-04-25 10:23:05 +0200

    Save state in local storage

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -2,18 +2,39 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-const initialState = {
-  modalPizza: { name: '', description: '', price: '' },
-  openModal: false,
-  cart: []
+const middleware = [thunk];
+
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (e) {
+    return undefined;
+  }
 };
 
-const middleware = [thunk];
+const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {
+    // Ignore write errors;
+  }
+};
+
+const persistedState = loadState();
 
 const store = createStore(
   rootReducer,
-  initialState,
+  persistedState,
   compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
