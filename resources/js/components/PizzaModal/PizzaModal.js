114d54fa6ed498c4c5d8ec55377b0b1bb5d681a5
Bojan Gabric <bojan@bojangabric.com>
2021-07-16 11:13:02 +0200

    feat: use headlessui modal

--- resources/js/components/PizzaModal/PizzaModal.js
+++ resources/js/components/PizzaModal/PizzaModal.js
@@ -1,170 +1,160 @@
-import React, { Component } from 'react';
-import Modal from 'react-modal';
+import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { addToCart, closeModal } from '../../actions';
 import Radiobutton from './Radiobutton';
 import Checkbox from './Checkbox';
+import { Dialog, Transition } from '@headlessui/react';
+import { MinusSmIcon, PlusSmIcon } from '@heroicons/react/solid';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    padding: '0',
-    boxShadow: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-    border: 'none'
-  }
-};
+const PizzaModal = ({ modalPizza, openModal, closeModal, addToCart }) => {
+  const [count, setCount] = useState(1);
+  const [size, setSize] = useState('24cm');
+  const [toppings, setToppings] = useState([]);
 
-class PizzaModal extends Component {
-  constructor(props) {
-    super(props);
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
 
-    this.state = {
-      count: 1,
-      toppings: [],
-      size: '24cm',
-      name: '',
-      price_eur: '',
-      price_dollar: '',
-      cartid: ''
-    };
+  const decrementCount = () => {
+    setCount(count + 1);
+    if (count > 1) setCount(count - 1);
+  };
 
-    this.onClose = this.onClose.bind(this);
-    this.onAddToCart = this.onAddToCart.bind(this);
-    this.radioChange = this.radioChange.bind(this);
-    this.checkboxChange = this.checkboxChange.bind(this);
-  }
+  const onClose = () => {
+    closeModal();
+    setCount(1);
+  };
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    this.setState({
-      name: nextProps.modalPizza.name,
-      price_eur: nextProps.modalPizza.price_eur,
-      price_dollar: nextProps.modalPizza.price_dollar,
+  const onAddToCart = () => {
+    addToCart({
+      count,
+      size,
+      toppings,
+      name: modalPizza.name,
+      price_eur: modalPizza.price_eur,
+      price_dollar: modalPizza.price_dollar,
       cartid: '_' + Math.random().toString(36).substr(2, 9)
     });
-  }
+    setCount(1);
+  };
 
-  incrementCount() {
-    this.setState(state => ({ count: state.count + 1 }));
-  }
+  const radioChange = e => {
+    setSize(e.target.value);
+  };
 
-  decrementCount() {
-    if (this.state.count > 1) this.setState(state => ({ count: state.count - 1 }));
-  }
-
-  onClose() {
-    this.props.closeModal();
-    this.setState({ count: 1 });
-  }
-
-  onAddToCart() {
-    this.props.addToCart(this.state);
-    this.setState({ count: 1 });
-  }
-
-  radioChange(e) {
-    this.setState({
-      size: e.target.value
-    });
-  }
-
-  checkboxChange(e) {
-    if (e.target.checked && !this.state.toppings.includes(e.target.value)) {
-      this.setState({
-        toppings: [...this.state.toppings, e.target.value]
-      });
+  const checkboxChange = e => {
+    if (e.target.checked && !toppings.includes(e.target.value)) {
+      setToppings([...toppings, e.target.value]);
     } else {
-      this.setState({
-        toppings: this.state.toppings.filter(topping => topping !== e.target.value)
-      });
+      setToppings([...toppings.filter(topping => topping !== e.target.value)]);
     }
-  }
+  };
 
-  render() {
-    return (
-      <Modal
-        isOpen={this.props.openModal}
-        onRequestClose={this.onClose}
-        overlayClassName="overlay-modal"
-        style={customStyles}
-        ariaHideApp={false}
-      >
-        <button
-          className="absolute top-0 right-0 mt-3 mr-3 bg-red-300 rounded-full p-1"
-          onClick={this.props.closeModal}
-        >
-          <svg strokeWidth="2" viewBox="0 0 24 24" className="text-red-900 stroke-current w-6 h-6">
-            <path d="M6 18L18 6M6 6l12 12"></path>
-          </svg>
-        </button>
-        <div className="max-w-2xl">
-          <div>
-            <img src={`/images/${this.props.modalPizza.name.toLowerCase()}.jpeg`} />
-          </div>
-          <div className="p-10">
-            <div className="flex justify-between">
-              <div className="font-medium text-xl">{this.props.modalPizza.name}</div>
-              <div className="text-blue-600">
-                {this.props.modalPizza.price_eur}€ / {this.props.modalPizza.price_dollar}$
-              </div>
-            </div>
-            <div className="text-sm font-light italic mt-1">{this.props.modalPizza.description}</div>
-            <div>
-              <div className="mt-4 mb-2">Size</div>
-              <div className="grid grid-cols-2 gap-5">
-                <Radiobutton onChange={this.radioChange} size="24cm" />
-                <Radiobutton onChange={this.radioChange} size="32cm" />
-              </div>
-            </div>
-            <div>
-              <div className="mt-4 mb-2">Extra toppings</div>
-              <div className="grid grid-cols-2 col-gap-5 row-gap-2">
-                <Checkbox onChange={this.checkboxChange} topping="Ham" />
-                <Checkbox onChange={this.checkboxChange} topping="Cheese" />
-                <Checkbox onChange={this.checkboxChange} topping="Bacon" />
-                <Checkbox onChange={this.checkboxChange} topping="Mushrooms" />
-              </div>
-            </div>
-            <div className="text-right flex items-center flex-row-reverse mt-6">
-              <button
-                onClick={this.onAddToCart}
-                className="ml-4 bg-yellow-400 text-yellow-800 mt-2 h-10 flex items-center px-6 rounded"
-              >
-                Add to cart
-              </button>
-              <div className="h-10 w-32">
-                <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
-                  <button
-                    onClick={() => this.decrementCount()}
-                    className="bg-white shadow text-gray-600 hover:text-black h-full w-20 rounded-l outline-none"
-                  >
-                    <span className="m-auto text-2xl font-thin">−</span>
-                  </button>
-                  <input
-                    disabled
-                    type="number"
-                    className="outline-none focus:outline-none text-center w-full bg-white shadow font-semibold text-md flex items-center text-gray-700"
-                    value={this.state.count}
-                  />
-                  <button
-                    onClick={() => this.incrementCount()}
-                    className="bg-white shadow text-gray-600 h-full w-20 hover:text-black rounded-r"
-                  >
-                    <span className="m-auto text-2xl font-thin">+</span>
-                  </button>
+  return (
+    <>
+      <Transition appear show={openModal} as={Fragment}>
+        <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" onClose={onClose}>
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span className="inline-block h-screen align-middle" aria-hidden="true">
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-2xl my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <button
+                  className="absolute top-0 right-0 mt-3 mr-3 bg-red-300 hover:bg-red-400 transition rounded-full p-1 focus:outline-none"
+                  onClick={closeModal}
+                >
+                  <svg strokeWidth="2" viewBox="0 0 24 24" className="text-red-900 stroke-current w-6 h-6">
+                    <path d="M6 18L18 6M6 6l12 12"></path>
+                  </svg>
+                </button>
+                <div className="max-w-2xl">
+                  <img src={`/images/${modalPizza.name.toLowerCase()}.jpeg`} />
+                  <div className="p-10">
+                    <div className="flex justify-between">
+                      <div className="font-medium text-xl">{modalPizza.name}</div>
+                      <div className="text-blue-600">
+                        {modalPizza.price_eur}€ / {modalPizza.price_dollar}$
+                      </div>
+                    </div>
+                    <div className="text-sm font-light italic mt-1">{modalPizza.description}</div>
+                    <div>
+                      <div className="mt-4 mb-2">Size</div>
+                      <div className="grid grid-cols-2 gap-5">
+                        <Radiobutton onChange={radioChange} enabled size="24cm" />
+                        <Radiobutton onChange={radioChange} size="32cm" />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="mt-4 mb-2">Extra toppings</div>
+                      <div className="grid grid-cols-2 gap-5">
+                        <Checkbox onChange={checkboxChange} topping="Ham" />
+                        <Checkbox onChange={checkboxChange} topping="Cheese" />
+                        <Checkbox onChange={checkboxChange} topping="Bacon" />
+                        <Checkbox onChange={checkboxChange} topping="Mushrooms" />
+                      </div>
+                    </div>
+                    <div className="text-right flex items-center flex-row-reverse mt-6">
+                      <button
+                        onClick={onAddToCart}
+                        className="ml-4 bg-yellow-400 text-yellow-800 mt-2 h-10 flex items-center px-6 rounded"
+                      >
+                        Add to cart
+                      </button>
+                      <div className="h-10 w-32">
+                        <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+                          <button
+                            onClick={() => decrementCount()}
+                            className="bg-white shadow text-gray-600 hover:text-black h-full w-20 rounded-l outline-none hover:bg-gray-100 transition"
+                          >
+                            <MinusSmIcon className="w-5 h-6 text-gray-600 m-auto" />
+                          </button>
+                          <input
+                            disabled
+                            type="number"
+                            className="outline-none focus:outline-none border-none text-center w-full bg-white shadow font-semibold text-md flex items-center text-gray-700"
+                            value={count}
+                          />
+                          <button
+                            onClick={() => incrementCount()}
+                            className="bg-white shadow text-gray-600 h-full w-20 hover:text-black rounded-r hover:bg-gray-100 transition"
+                          >
+                            <PlusSmIcon className="w-5 h-6 text-gray-600 m-auto" />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Transition.Child>
           </div>
-        </div>
-      </Modal>
-    );
-  }
-}
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
 
 const mapStateToProps = state => ({
   openModal: state.openModal,
