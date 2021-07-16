7055af3323e9bd4c8709b7b11fe6b1a58c4effbd
Bojan Gabric <bojan@bojangabric.com>
2021-07-16 11:12:49 +0200

    feat: update checkbox and radio button inputs

--- resources/js/components/PizzaModal/Checkbox.js
+++ resources/js/components/PizzaModal/Checkbox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Checkbox = ({ topping, onChange }) => {
   return (
     <div>
-      <label className="bg-white flex shadow p-4 items-center rounded text-sm">
+      <label className="bg-white flex border p-4 items-center rounded text-sm">
         <input className="form-checkbox" type="checkbox" value={topping} onChange={e => onChange(e)} />
         <span className="ml-2">{topping}</span>
       </label>
--- resources/js/components/PizzaModal/Radiobutton.js
+++ resources/js/components/PizzaModal/Radiobutton.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const Radiobutton = ({ size, onChange }) => {
+const Radiobutton = ({ size, onChange, enabled }) => {
   return (
     <div>
-      <label className="bg-white flex shadow p-4 items-center rounded text-sm">
-        <input className="form-radio" type="radio" name="size" value={size} onChange={e => onChange(e)} />
+      <label className="bg-white flex border p-4 items-center rounded text-sm">
+        <input
+          className="form-radio"
+          type="radio"
+          name="size"
+          checked={enabled}
+          value={size}
+          onChange={e => onChange(e)}
+        />
         <span className="ml-2">{size}</span>
       </label>
     </div>
