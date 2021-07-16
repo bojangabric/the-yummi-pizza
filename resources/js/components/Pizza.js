0bccaf2b796c676feab94b2327ffdd49f6ef5d0e
Bojan Gabric <bojan@bojangabric.com>
2021-07-16 11:23:56 +0200

    style: update pizza card

--- resources/js/components/Pizza.js
+++ resources/js/components/Pizza.js
@@ -1,3 +1,4 @@
+import { PlusSmIcon } from '@heroicons/react/solid';
 import React from 'react';
 import { connect } from 'react-redux';
 import { openModal } from '../actions';
@@ -7,21 +8,22 @@ const Pizza = props => {
   const img = `/images/${name.toLowerCase()}.jpeg`;
 
   return (
-    <div className="shadow rounded overflow-hidden flex flex-col flex-grow">
-      <div className="w-full">
-        <img src={img} />
-      </div>
-      <div className="p-5 flex flex-col flex-grow">
-        <div className="font-medium">{name}</div>
-        <div className="text-sm font-light italic mt-1 flex-grow">{description}</div>
-        <div className="mt-5 justify-between flex items-center">
-          <div className="text-blue-600">
+    <div className="flex flex-col flex-grow">
+      <img src={img} className="w-full rounded" />
+      <div className="py-4 flex flex-col flex-grow">
+        <div className="flex justify-between">
+          <div className="font-medium">{name}</div>
+          <div>
             {price_eur}€ / {price_dollar}$
           </div>
-          <div className="cursor-pointer" onClick={() => openModal(props)}>
-            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6">
-              <path d="M12 4V20M20 12L4 12" stroke="#3182ce" strokeWidth="2" />
-            </svg>
+        </div>
+        <div className="text-sm font-light italic mt-4 flex-grow">{description}</div>
+        <div className="mt-4 justify-between flex items-center">
+          <div
+            className="cursor-pointer inline-flex items-center text-blue-600 hover:text-blue-800 transition"
+            onClick={() => openModal(props)}
+          >
+            <PlusSmIcon className="w-6 h-6" /> Add to cart
           </div>
         </div>
       </div>
